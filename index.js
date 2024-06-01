253944a083c5deaabfd967be8d2028214c18218d
itsahadul99 <itsahadul999@gmail.com>
2024-06-01 17:35:33 +0600

    added contest details api

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cors(corsOptions))
 app.use(express.json())
 app.use(cookieParser())
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ifklbg0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -90,6 +90,13 @@ async function run() {
         app.get('/contests', async (req, res) => {
             const result = await contestsCollection.find().toArray()
             res.send(result)
+        })
+        // get a single data for details 
+        app.get('/contestDetails/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await contestsCollection.findOne(query)
+            res.send(result);
         })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
